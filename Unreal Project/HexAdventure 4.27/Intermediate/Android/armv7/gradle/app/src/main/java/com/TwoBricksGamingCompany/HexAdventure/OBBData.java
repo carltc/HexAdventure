22f4daced401886bbb56257f685cee6daa6a3dc1
Carl Chittenden <carltc@hotmail.co.uk>
2023-02-27 13:13:28 -0800

    [FIX] Corrected naming of visibility and discovery distances

--- Unreal Project/HexAdventure 4.27/Intermediate/Android/armv7/gradle/app/src/main/java/com/TwoBricksGamingCompany/HexAdventure/OBBData.java
+++ Unreal Project/HexAdventure 4.27/Intermediate/Android/armv7/gradle/app/src/main/java/com/TwoBricksGamingCompany/HexAdventure/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-43869883L // the length of the file in bytes
+43865465L // the length of the file in bytes
 )
 };
 };
