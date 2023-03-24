2456b1965b3341e18a0412ea74ae25590ac962d7
Carl Chittenden <carltc@hotmail.co.uk>
2023-03-24 13:29:23 -0700

    [ADD] Adding missing CPP files as well as new models for Meeple and Buildings

---
+++ Source/HexAdventure/MultithreadedLibrary.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+#include "Async/AsyncWork.h"
+#include "Multithreaded.h"
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MultithreadedLibrary.generated.h"
+class MultithreadedTask : public FNonAbandonableTask {
+public:
+	MultithreadedTask(UObject* object) { this->object = object; }
+	UObject* object;
+	FORCEINLINE TStatId GetStatId() const {
+		RETURN_QUICK_DECLARE_CYCLE_STAT(MultithreadedTask, STATGROUP_ThreadPoolAsyncTasks);
+	}
+	void DoWork()
+	{
+		IMultithreaded::Execute_MultithreadedFunction(object);
+	}
+};
+/**
+ *
+ */
+UCLASS()
+class HEXADVENTURE_API UMultithreadedLibrary : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+		static void CallMultithreadedFunction(UObject* object) {
+		(new FAutoDeleteAsyncTask<MultithreadedTask>(object))->StartBackgroundTask();
+	}
+};
